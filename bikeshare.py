d0e7b4782152bc721d879055e1acdc4e473b77fe
joshua_roberge <jwr1015@outlook.com>
2022-05-10 18:46:48 -0400

    cleaned doc strings in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -77,6 +77,7 @@ def load_data(city, month, day):
         (str) city - name of the city to analyze
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
+
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
